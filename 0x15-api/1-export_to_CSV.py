ce81c7b5727ae3372c83c4728e68beece238881f
leokingue <ebangkingue@icloud.com>
2023-02-13 05:39:07 -0800

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,9 +17,8 @@ if __name__ == "__main__":
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
                     file.write(
